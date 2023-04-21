a3bdcdb707c6a325f3a925149069dd8e97f1f148
Aryansh <aryanshs@buffalo.edu>
2023-04-21 16:33:17 -0400

    minor changes to url

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from contextlib import redirect_stderr
 from distutils.log import error
 from http import client
 from urllib import request
-from flask import Flask, render_template, url_for, request, session
+from flask import Flask, render_template, url_for, request, session, redirect
 from pymongo import MongoClient
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Email, Length, ValidationError
@@ -34,7 +34,8 @@ def signUp():
         data = request.form.to_dict()
 
         pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
-        # if not re.match(pattern, data['password']):
+
+        # checking if password is strong enough
         if len(data['password']) < 8 or not re.match(pattern, data['password']):
             errorMessage = 'Password must contain at least 8 characters, at least one uppercase letter, one lowercase letter, one number, and one special character.'
             return render_template('signup.html', name=data['name'], username=data['username'], email=data['email'], error=errorMessage)
@@ -42,15 +43,27 @@ def signUp():
             errorMessage = 'Passwords must be the same'
             return render_template('signup.html', name=data['name'], username=data['username'], email=data['email'], error=errorMessage)
         else:
+            data['noContent'] = True
             hatTop.insert_one(data)
-            return render_template('professorOrStudent.html')
+            # this can be called from anywhere
+            session['username'] = data['username']
+
+            return redirect(url_for('profStudent'))
 
     return render_template('signup.html')
 
 
 @app.route('/home')
 def homePage():
-    return render_template('homePage.html')
+    # checking if user is a professor or a student, and checking if they are enrolled or have signed up for any classes
+    userData = hatTop.find_one({'username': session.get('username')})
+    if 'professor' in userData:
+        if 'noContent' in userData:
+            return render_template('homePage.html', professor=True, noContent=True)
+
+    if 'student' in userData:
+        if 'noContent' in userData:
+            return render_template('homePage.html', student=True, noContent=True)
 
 
 @app.route('/professororstudent', methods=['GET', 'POST'])
@@ -59,18 +72,22 @@ def profStudent():
         data = request.form.to_dict()
         print(data)
         print(session.get('username'))
+
+        # setting professor and student to true or false for the user to identify if they they are a professor or a student
         if 'professor' in data:
             userData = hatTop.find_one({'username': session.get('username')})
             userData['professor'] = True
             # update it in database
             hatTop.update_one({'_id': userData['_id']}, {'$set': userData})
-            return render_template('homePage.html', professor=True)
+
+            return redirect(url_for('homePage'))
         if 'student' in data:
             userData = hatTop.find_one({'username': session.get('username')})
             userData['student'] = True
             # update it in database
             hatTop.update_one({'_id': userData['_id']}, {'$set': userData})
-            return render_template('homePage.html', student=True)
+
+            return redirect(url_for('homePage'))
 
     return render_template('professorOrStudent.html')
 
@@ -78,7 +95,7 @@ def profStudent():
 @app.route('/login', methods=['GET', 'POST'])
 def loginPhase2():
     if request.method == "POST":
-        data = request.form.to_dict()
+        data = request.form.to_dict()  # converting the post data into a dictionary
         schoolData = ""
         if data['schoolData']:
             schoolData = data['schoolData']
@@ -86,9 +103,15 @@ def loginPhase2():
         userData = hatTop.find_one({'username': data['username']})
         # this can be called from anywhere
         session['username'] = data['username']
+
+        # checking if user has an account
+
+        # if they don't we send them a link to sign up page
         if userData == None:
             error_message = "We don't have an account with that username, please create an account."
             return render_template('loginPhase2.html', AcctError=error_message)
+
+        # if they do have an account, we check if their password, username, and school are correctly entered
         else:
             if userData['password'] != data['password']:
                 error_message = "Please Check Your Password"
@@ -98,15 +121,23 @@ def loginPhase2():
                 return render_template('loginPhase2.html', schoolSelected=userData['schoolData'], username=data['username'], SchoolError=error_message)
             else:
 
-                userData = hatTop.find_one(
-                    {'username': session.get('username')})
-                if 'professor' in userData:
-                    return render_template('homePage.html', professor=True)
-                if 'student' in userData:
-                    return render_template('homePage.html', student=True)
+                # if everything was correctly entered, we render the homepage
+                return redirect(url_for('homePage'))
 
         print(userData)
     return render_template('loginPhase2.html')
+
+
+# checking if user is logged in
+
+
+def userLoggedIn():
+    username = session.get('username', None)
+
+    if username is not None:
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
