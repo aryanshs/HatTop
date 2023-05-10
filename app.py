ad3558070df11883ffbc07c86e2f9898f5269a4c
ZackaryHeraghty <Zackary.Heraghty@Gmail.com>
2023-05-10 18:49:54 -0400

    student can answer questions via socket

--- app.py
+++ app.py
@@ -282,7 +282,7 @@ def coursePage():
 
 
     if 'student' in userData:
-        return render_template('coursePage.html', qid=question['_id'], student=True,courseName=course['coursePrefix'], courseID=cid, activeQuestions=activeQuestions)
+        return render_template('coursePage.html', cid=cid,qid=question['_id'], student=True,courseName=course['coursePrefix'], courseID=cid, activeQuestions=activeQuestions)
 
 from bson.objectid import ObjectId
 #receives completed createquestion forms
@@ -315,11 +315,16 @@ def activequestion():
             return render_template('loginPage.html')
         
     #if it's a get then use question id to get information from db
-    qid = request.args.get('id')
-    cid = request.args.get('courseID')
+    qid = request.args.get('qid')
+    cid = request.args.get('cid')
     obj = ObjectId(qid)
     q = questions.find_one({'_id': obj})
     
+    #get courseData
+    obj = ObjectId(cid)
+    course = professorAndStudents.find_one({'_id': obj})
+
+
     #get the answers
     ans1 = q['answers'][0]
     ans2 = q['answers'][1]
@@ -333,7 +338,7 @@ def activequestion():
     if(len(q['answers']) >= 5):
         ans5 = q['answers'][4]
 
-    return render_template('activeQuestion.html',questionID=obj,courseID=cid,courseName=q['course'], student=True,question=q['question'] , answer1=ans1, answer2=ans2, answer3=ans3, answer4=ans4, answer5=ans5)
+    return render_template('activeQuestion.html',questionID=qid,courseID=cid,courseName=course['coursePrefix'], student=True,question=q['question'] , answer1=ans1, answer2=ans2, answer3=ans3, answer4=ans4, answer5=ans5)
     
 #upon ending a question:
 #1. Set the question to inactive in DB
@@ -377,6 +382,7 @@ def handleSubmission(answerInfo):
     answer = answerInfo['answer']
     questionID = answerInfo['questionID']
     print('submission received')
+    print('qid: ', questionID)
     question = questions.find_one({'_id':ObjectId(questionID)})
 
     #If question is inactive then tell client
@@ -386,13 +392,13 @@ def handleSubmission(answerInfo):
         socket.emit('questionClosed')
     
     #check if answer is correct
-    if(answer == question['correctAnswer']):
+    if(answer == "answer" + str(question['correctAnswer'])):
         score = 1
     else:
         score = 0
 
     #perform a upsert
-    data = {'course':question['course'], 'answer':answer, 'score':score, 'question':question['question']}
+    data = {'cid':question['cid'], 'answer':answer, 'score':score, 'question':question['question']}
     gradeBook.update_one({'qid':questionID, 'student':session.get('username')}, {"$set": data}, upsert=True)
     
     #get the number of subissions for question
